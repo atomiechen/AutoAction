ea2914a31792bd644e794e1398e1bc68f9caa97b
王政 <zhengwan20@mails.tsinghua.edu.cn>
2022-07-22 15:06:49 +0800

    extend app list

--- android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/contextaction/context/ConfigContext.java
+++ android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/contextaction/context/ConfigContext.java
@@ -68,6 +68,7 @@ public class ConfigContext extends BaseContext {
     private String latest_deviceType;
     private List<String> valid_packageNames;
     private List<String> useless_packageNames;
+    private List<String> nochange_packageNames;
     private List<String> rules;
     private int brightness;
     private final HashMap<String, Integer> volume;
@@ -121,6 +122,7 @@ public class ConfigContext extends BaseContext {
                 "com.tencent.qqmusic", //QQ音乐
                 "com.tencent.wemeet.app", //腾讯会议
                 "com.baidu.searchbox", //百度
+                "com.baidu.searchbox.lite",
                 "com.youku.phone", //优酷视频
                 "com.netease.cloudmusic", //网易云音乐
                 "com.gotokeep.keep", //Keep
@@ -138,10 +140,93 @@ public class ConfigContext extends BaseContext {
                 "com.kuaishou.nebula", //快手极速版
                 "com.qiyi.video.lite", //爱奇艺极速版
                 "com.ss.android.ugc.aweme.lite", //抖音极速版
-                "com.ss.android.ugc.live", //火山小视频
+                "com.ss.android.ugc.live", //抖音火山版
                 "air.tv.douyu.android", //斗鱼
                 "com.duowan.kiwi", //虎牙
-                "com.sohu.sohuvideo"); //搜狐视频
+                "com.sohu.sohuvideo", //搜狐视频
+                "com.autonavi.minimap", //高德地图
+                "cn.soulapp.android",
+                "com.kugou.android",
+                "com.immomo.momo",
+                "com.ss.android.article",
+                "com.ss.android.article.news",
+                "com.ss.android.article.lite",
+                "com.ss.android.auto",
+                "com.cubic.autohome",
+                "com.xs.fm",
+                "com.le123.ysdq",
+                "com.ximalaya.ting.android",
+                "com.p1.mobile.putong",
+                "com.hunantv.imgo.activity",
+                "com.taobao.litetao",
+                "com.tencent.wework",
+                "com.tmall.wireless",
+                "com.taobao.live",
+                "cn.xuexi.android",
+                "com.tencent.map",
+                "com.tencent.gamehelper.smoba",
+                "com.tencent.gamehelper.pg",
+                "com.mihoyo.hyperion",
+                "com.tencent.tmgp.sgame",
+                "com.tencent.tmgp.pubgmhd",
+                "com.miHoYo.GenshinImpact",
+                "com.tencent.jkchess",
+                "com.tencent.lolm",
+                "com.netease.sky.aligames",
+                "com.knight.union.aligames",
+                "com.tencent.tmgp.supercell.brawlstars",
+                "com.miHoYo.bh3.uc",
+                "com.quark.browser",
+                "com.UCMobile",
+                "com.tencent.mtt",
+                "com.huawei.health",
+                "com.douban.frodo",
+                "cn.ledongli.ldl",
+                "com.taptap",
+                "com.jingdong.app.mall",
+                "com.jd.jdlite",
+                "com.achievo.vipshop",
+                "com.xunmeng.pinduoduo",
+                "org.mozilla.firefox",
+                "com.microsoft.emmx",
+                "com.android.chrome",
+                "com.android.browser",
+                "com.hicloud.browser",
+                "com.huawei.browser",
+                "com.dianping.v1",
+                "com.taobao.idlefish",
+                "com.baidu.BaiduMap"); //百度地图
+        nochange_packageNames = Arrays.asList(
+                "com.sankuai.meituan",
+                "com.sankuai.meituan.takeoutnew",
+                "com.dragon.read",
+                "com.eg.android.AlipayGphone",
+                "com.wuba",
+                "me.ele",
+                "com.lemon.lv",
+                "com.baidu.netdisk",
+                "com.alibaba.android.rimet",
+                "com.kmxs.reader",
+                "com.xunlei.downloadprovider",
+                "com.baidu.homework",
+                "com.hpbr.bosszhipin",
+                "com.shizhuang.duapp",
+                "com.wuba.zhuanzhuan",
+                "ctrip.android.view",
+                "com.Qunar",
+                "com.mt.mtxx.mtxx",
+                "com.anjuke.android.app",
+                "com.handsgo.jiakao.android",
+                "com.zhaopin.social",
+                "com.jingyao.easybike",
+                "cn.wps.moffice_eng",
+                "com.didapinche.booking",
+                "com.MobileTicket",
+                "com.icbc",
+                "com.intsig.camscanner",
+                "com.chinamworld.bocmbci",
+                "cn.tape.tapeapp"
+                );
     }
 
     @Override
@@ -181,28 +266,30 @@ public class ConfigContext extends BaseContext {
             String tmp_name = event.getPackageName().toString();
             if (!useless_packageNames.contains(tmp_name))
                 present_name = tmp_name;
-            if (valid_packageNames.contains(present_name)) {
+            if (valid_packageNames.contains(present_name) || nochange_packageNames.contains(present_name)) {
                 packageName = present_name;
                 if (!(packageName.equals(last_packageName))) {
-                    long now = System.currentTimeMillis();
-                    int logID = incLogID();
-                    notifyContext(NEED_AUDIO, now, logID, "app changed: " + packageName);
-                    notifyContext(NEED_SCAN, now, logID, "app changed: " + packageName);
-                    notifyContext(NEED_POSITION, now, logID, "app changed: " + packageName);
-                    Date date = new Date();
-                    double latitude = GPSCollector.latest_data.getLatitude();
-                    double longitude = GPSCollector.latest_data.getLongitude();
-                    double noise = AudioCollector.lastest_noise;
-                    String app = packageName;
-                    String deviceType = latest_deviceType;
-                    rules = getRules(date, latitude, longitude, noise, app, deviceType);
-                    List<Integer> volumes = getVolumes(date, latitude, longitude, noise, app, deviceType);
-                    if (requestListener != null) {
-                        RequestConfig requestConfig = new RequestConfig();
-                        requestConfig.putValue("needVolumeOverlay", true);
-                        requestConfig.putString("rules", rules.toString());
-                        requestConfig.putString("volumes", volumes.toString());
-                        requestListener.onRequest(requestConfig);
+                    if (valid_packageNames.contains(packageName)) {
+                        long now = System.currentTimeMillis();
+                        int logID = incLogID();
+                        notifyContext(NEED_AUDIO, now, logID, "app changed: " + packageName);
+                        notifyContext(NEED_SCAN, now, logID, "app changed: " + packageName);
+                        notifyContext(NEED_POSITION, now, logID, "app changed: " + packageName);
+                        Date date = new Date();
+                        double latitude = GPSCollector.latest_data.getLatitude();
+                        double longitude = GPSCollector.latest_data.getLongitude();
+                        double noise = AudioCollector.lastest_noise;
+                        String app = packageName;
+                        String deviceType = latest_deviceType;
+                        rules = getRules(date, latitude, longitude, noise, app, deviceType);
+                        List<Integer> volumes = getVolumes(date, latitude, longitude, noise, app, deviceType);
+                        if (requestListener != null) {
+                            RequestConfig requestConfig = new RequestConfig();
+                            requestConfig.putValue("needVolumeOverlay", true);
+                            requestConfig.putString("rules", rules.toString());
+                            requestConfig.putString("volumes", volumes.toString());
+                            requestListener.onRequest(requestConfig);
+                        }
                     }
                     last_packageName = packageName;
                 }
@@ -217,7 +304,7 @@ public class ConfigContext extends BaseContext {
         for (VolumeRule volumeRule: volumeRules) {
             _rules.add(volumeRule.getType().getText() + " volume=" + volumeRule.getVolume());
         }
-        _rules.add(date.toString() + " (" + latitude + "/" + longitude + ") " + noise + " " + app + " " + deviceType + " " + volumeRuleManager.getContextListSize());
+        _rules.add(date.toString() + " (" + latitude + "/" + longitude + ") " + noise + " " + app + " " + last_packageName + " " + deviceType + " " + volumeRuleManager.getContextListSize());
         return _rules;
     }
 
