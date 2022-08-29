b870d0b57f6bad82cc4b1bb92102d1a05b24d2cb
Atomie CHEN <atomic_cwh@163.com>
2022-08-29 19:08:09 +0800

    (Incomplete) add audio recording function to SoundManager

--- android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/volume/SoundManager.java
+++ android/DataCollection/ContextActionLibrary/src/main/java/com/hcifuture/contextactionlibrary/volume/SoundManager.java
@@ -1,21 +1,38 @@
 package com.hcifuture.contextactionlibrary.volume;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioFormat;
 import android.media.AudioManager;
+import android.media.AudioPlaybackCaptureConfiguration;
+import android.media.AudioRecord;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 
 public class SoundManager extends TriggerManager {
 
-    private double NORMALIZED_MAX_VOLUME = 100.0;
+    private final double NORMALIZED_MAX_VOLUME = 100.0;
 
-    private AudioManager audioManager;
-    private Context mContext;
-    private int MAX_VOLUME_MUSIC;
+    private final AudioManager audioManager;
+
+    private final MediaProjectionManager mediaProjectionManager;
+    private MediaProjection mediaProjection;
+    private AudioRecord audioRecord;
+
+    private final Context mContext;
+    private final int MAX_VOLUME_MUSIC;
 
     public SoundManager(VolEventListener volEventListener, Context context) {
         super(volEventListener);
         mContext = context;
         audioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
         MAX_VOLUME_MUSIC = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        mediaProjectionManager = (MediaProjectionManager) mContext.getSystemService(Context.MEDIA_PROJECTION_SERVICE);
     }
 
     public boolean isAudioOn() {
@@ -32,16 +49,48 @@ public class SoundManager extends TriggerManager {
         return audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
     }
 
-    public double getNormalizedVolume() {
-        return getVolume() * NORMALIZED_MAX_VOLUME / MAX_VOLUME_MUSIC;
-    }
-
     public int percent2int(double vol_percent) {
-        return (int)(vol_percent * MAX_VOLUME_MUSIC / 100 + 0.5d);
+        return (int)(vol_percent * MAX_VOLUME_MUSIC / NORMALIZED_MAX_VOLUME + 0.5d);
     }
 
     public double int2percent(int vol) {
-        return vol * 100.0 / MAX_VOLUME_MUSIC;
+        return vol * NORMALIZED_MAX_VOLUME / MAX_VOLUME_MUSIC;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @SuppressLint("MissingPermission")
+    public void startAudioCapture(int resultCode, Intent data) {
+        mediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data);
+        if (mediaProjection != null) {
+            // TODO
+            AudioPlaybackCaptureConfiguration audioPlaybackCaptureConfiguration =
+                    new AudioPlaybackCaptureConfiguration.Builder(mediaProjection)
+                    .addMatchingUsage(AudioAttributes.USAGE_MEDIA)
+                    .build();
+
+            AudioFormat audioFormat = new AudioFormat.Builder()
+                    .setEncoding(AudioFormat.ENCODING_AAC_LC)
+                    .setSampleRate(8000)
+                    .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
+                    .build();
+
+            audioRecord = new AudioRecord.Builder()
+                    .setAudioPlaybackCaptureConfig(audioPlaybackCaptureConfiguration)
+                    .build();
+
+            audioRecord.startRecording();
+        }
+    }
+
+    public void stopAudioCapture() {
+        if (audioRecord != null) {
+            audioRecord.stop();
+            audioRecord.release();
+            audioRecord = null;
+        }
+        if (mediaProjection != null) {
+            mediaProjection.stop();
+            mediaProjection = null;
+        }
+    }
 }
